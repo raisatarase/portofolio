140a1eafefeca99685d0532b39cb8044031c6a34
Raisa Tarase <raisa.tarase@gmail.com>
2023-09-18 21:33:22 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Get the current year for the footer
+const currentYear = new Date().getFullYear();
+
+// Update the content of the span element with the current year
+document.getElementById("currentYear").textContent = currentYear;
